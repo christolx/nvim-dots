59ed61c3a60e1a9709aba825b5f58cf375ca51be
christolx <81254243+christolx@users.noreply.github.com>
2026-05-07 12:17:52 +0700

    feat(snacks): added recursive folder expansion for explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      opts.picker = opts.picker or {}
+      opts.picker.sources = opts.picker.sources or {}
+      opts.picker.sources.explorer = opts.picker.sources.explorer or {}
+
+      local explorer = opts.picker.sources.explorer
+      explorer.actions = explorer.actions or {}
+      explorer.win = explorer.win or {}
+      explorer.win.list = explorer.win.list or {}
+      explorer.win.list.keys = explorer.win.list.keys or {}
+
+      explorer.actions.explorer_expand_all = function(picker)
+        local Tree = require("snacks.explorer.tree")
+        local Actions = require("snacks.explorer.actions")
+        local root = Tree:find(picker:cwd())
+        local filter = Tree:filter(picker.opts)
+
+        Tree:walk(root, function(node)
+          if node ~= root and not filter(node) then
+            return false
+          end
+
+          if node.dir then
+            node.open = true
+            if not node.expanded then
+              Tree:expand(node)
+            end
+          end
+        end, { all = true })
+
+        Actions.update(picker, { refresh = true })
+      end
+
+      explorer.win.list.keys["zR"] = { "explorer_expand_all", desc = "Expand All" }
+    end,
+  },
+}
