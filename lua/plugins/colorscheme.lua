8f2951e4bbb40a60326b6508ed0e1182b7ad426b
christolx <81254243+christolx@users.noreply.github.com>
2026-05-09 14:22:20 +0700

    feat: added a bunch of themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,64 @@
 return {
   {
     "folke/tokyonight.nvim",
+    priority = 1000,
     opts = {
-      style = "night",
       terminal_colors = false,
     },
   },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    opts = {
+      -- your current line should be under `options`
+      options = {
+        terminal_colors = false,
+      },
 
-  { "LazyVim/LazyVim", opts = {
-    colorscheme = "tokyonight",
-  } },
+      colors = {
+        my_warn = "#e5c07b",
+        my_warn_bg = "#2b2518",
+      },
+
+      highlights = {
+        -- main diagnostic warning color
+        DiagnosticWarn = { fg = "${my_warn}" },
+
+        -- right-side text: "Expression value is unused"
+        DiagnosticVirtualTextWarn = {
+          fg = "${my_warn}",
+          bg = "${my_warn_bg}",
+        },
+
+        -- gutter/sign icon
+        DiagnosticSignWarn = { fg = "${my_warn}" },
+
+        -- squiggly underline
+        DiagnosticUnderlineWarn = {
+          sp = "${my_warn}",
+          undercurl = true,
+        },
+
+        -- floating diagnostic window warning text
+        DiagnosticFloatingWarn = { fg = "${my_warn}" },
+      },
+    },
+  },
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    init = function()
+      vim.o.termguicolors = true
+      vim.g.gruvbox_material_background = "hard" -- hard | medum | soft
+      vim.g.gruvbox_material_foreground = "material" -- material | mix | original
+      vim.g.gruvbox_material_enable_italic = 1
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark_dark",
+    },
+  },
 }
